3db6b1f80a53e079d0637f3f9650f4997467adca
Olof233 <1318817412@qq.com>
2025-11-24 18:13:22 +0800

    req

--- preprocessing/extract.py
+++ preprocessing/extract.py
@@ -3,10 +3,15 @@ from nltk.corpus import stopwords
 from sentence_transformers import SentenceTransformer
 import sys
 sys.path.append("..")
+from retrieval.bm25 import chinese_tokenizer
+from sklearn.feature_extraction.text import CountVectorizer
 
 
-def extract(text, n_range=((1, 3))):
-    model = SentenceTransformer('bert-base-chinese-sentence-transformer-xnli-zh')
+
+def extract(text):
+    vectorizer = CountVectorizer(tokenizer=chinese_tokenizer, ngram_range=(1, int(len(chinese_tokenizer(text))/2)))
+    model = SentenceTransformer('bert-base-chinese-sentence-transformer-xnli-zh', local_files_only=True)
     kw_model = KeyBERT(model)
-    keywords = kw_model.extract_keywords(text, keyphrase_ngram_range=n_range, stop_words=stopwords.words('chinese'))
+    keywords = kw_model.extract_keywords(text, vectorizer=vectorizer)
+    print(keywords, '\n')
     return keywords[0][0]
